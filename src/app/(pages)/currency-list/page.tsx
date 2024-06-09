161d042f20d3d6b904962edd84d3eb253888118e
user3 <user3@vdi.local>
2024-06-09 00:38:44 -0700

    disable datatable

---
+++ src/app/(pages)/currency-list/page.tsx
@@ -0,0 +1,15 @@
+import { DataTable } from '@/app/components/ui/data-table'
+import React from 'react'
+
+const CurrencyListPage = () => {
+
+
+  return (
+    <>
+        <h1>CurrencyListPage</h1>
+        {/*<DataTable  />*/}
+    </>
+  )
+}
+
+export default CurrencyListPage
